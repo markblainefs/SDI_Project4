08f371613e8b4dde7e71c42b13bb9f111b30b6e9
Mark Blaine <markopollo@unknown68a86d4158f4.gateway.2wire.net>
2012-06-19 22:26:58 -0500

    Added function to check phone number

--- blaine_mark_assignment4.js
+++ blaine_mark_assignment4.js
@@ -1,1 +1,25 @@
-alert("JavaScript works!");
+// Project 4: My Library
+// Mark A. Blaine
+// SDI 1206
+
+var markoLibrary = function(){
+	
+	// CHECK PHONE NUMBER FORMAT
+	// Uses a regular expression to look for three digits followed by a dash, followed by another three digits, a dash and four more digits.  The first digit of the area code has to be a 2 or higher, since area codes do not start with 1 or 0.
+	var checkPhoneNumber = function (number){
+		var target = /^([2-9]?)([0-9]{2})[-]?([0-9]{3})[-]?([0-9]{4})$/;
+		if (target.test(number)) {
+			return true;
+		} else {
+			return false;
+		};
+	};
+		
+	// RETURNS
+	return {
+		"checkPhoneNumber":checkPhoneNumber
+	};
+};
+
+var lib = new markoLibrary();
+console.log(lib.checkPhoneNumber("210-555-4444"));
