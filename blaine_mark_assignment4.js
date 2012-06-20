bd254623e2a314fa43f756b97c21cc44c91e783c
Mark Blaine <markopollo@unknown68a86d4158f4.gateway.2wire.net>
2012-06-19 22:40:18 -0500

    Added function to check email address

--- blaine_mark_assignment4.js
+++ blaine_mark_assignment4.js
@@ -6,20 +6,33 @@ var markoLibrary = function(){
 	
 	// CHECK PHONE NUMBER FORMAT
 	// Uses a regular expression to look for three digits followed by a dash, followed by another three digits, a dash and four more digits.  The first digit of the area code has to be a 2 or higher, since area codes do not start with 1 or 0.
-	var checkPhoneNumber = function (number){
+	var checkPhoneNumber = function(number){
 		var target = /^([2-9]?)([0-9]{2})[-]?([0-9]{3})[-]?([0-9]{4})$/;
-		if (target.test(number)) {
+		if (target.test(number)){
 			return true;
 		} else {
 			return false;
 		};
 	};
-		
+
+	// CHECK EMAIL FORMAT
+	// Uses a regular expression to look for any upper or lower case letters, number and dashes, periods or underscores with an "@" somewhere in the middle and ending with a top level domain of 2-4 letters.
+	var checkEmailAddress = function(email){
+		var target = /^[a-zA-Z0-9._-]+@[a-zA_Z0-9._-]+\.([a-zA_Z]{2,4})$/
+		if (target.test(email)){
+			return true;
+		} else {
+			return false;
+		};
+	};
+
 	// RETURNS
 	return {
-		"checkPhoneNumber":checkPhoneNumber
+		"checkPhoneNumber":checkPhoneNumber,
+		"checkEmailAddress":checkEmailAddress
 	};
 };
 
 var lib = new markoLibrary();
-console.log(lib.checkPhoneNumber("210-555-4444"));
+console.log("Phone number format? " + lib.checkPhoneNumber("210-555-4444"));
+console.log("Email address format? " + lib.checkEmailAddress("mark@markblaine.com"));
